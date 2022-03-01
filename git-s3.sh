e43db28d9c7bcc7101ae2b797d3c35b85ac78c24
Pavan <24794698+pavanchow@users.noreply.github.com>
2022-03-02 01:46:01 +0530

    Update git-s3.sh

--- git-s3.sh
+++ git-s3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pwd
 cd /home/runner/work/$github_repository/$github_repository/ph3-lab-mgmt/
 aws s3 sync exprepos/ s3://$BUCKET_ORG_S3_CS_TEST/ --recursive --include "*.html" --include "*.js" --include "*.css" --delete
 # --delete flag will remove any file in the s3 that are not on the new build
