a2575eff8e1f1868635679ce1f53789c254788d2
Pavan <24794698+pavanchow@users.noreply.github.com>
2022-03-02 02:04:53 +0530

    Update git-s3.sh

--- git-s3.sh
+++ git-s3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pwd
 cd ../
+ls -ltr
 aws s3 sync exprepos/ s3://$BUCKET_ORG_S3_CS_TEST/ --include "*.html" --include "*.js" --include "*.css" --delete
 # --delete flag will remove any file in the s3 that are not on the new build
 
