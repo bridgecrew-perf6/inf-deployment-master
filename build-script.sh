eaa753727abb8f2674dc33b8a92d6694496b311e
Pavan <24794698+pavanchow@users.noreply.github.com>
2022-03-02 01:00:07 +0530

    Update build-script.sh

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone --depth=1 -b s3-fix https://github.com/virtual-labs/ph3-lab-mgmt
+cd ../ && git clone --depth=1 -b s3-fix https://github.com/virtual-labs/ph3-lab-mgmt
 cd ph3-lab-mgmt
 git checkout s3-fix
 npm install
