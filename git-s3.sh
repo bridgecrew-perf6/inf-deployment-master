ae517ccabc3f8d491e2da0538c3651d454576a33
Pavan <24794698+pavanchow@users.noreply.github.com>
2022-03-02 01:48:48 +0530

    Update git-s3.sh

--- git-s3.sh
+++ git-s3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pwd
-cd /home/runner/work/$github_repository/$github_repository/ph3-lab-mgmt/
-aws s3 sync exprepos/ s3://$BUCKET_ORG_S3_CS_TEST/ --recursive --include "*.html" --include "*.js" --include "*.css" --delete
+cd /home/runner/work/$github_repository/$github_repository/inf-deployment-masterph3-lab-mgmt/
+aws s3 sync exprepos/ s3://$BUCKET_ORG_S3_CS_TEST/ --include "*.html" --include "*.js" --include "*.css" --delete
 # --delete flag will remove any file in the s3 that are not on the new build
 
 # aws cloudfront create-invalidation --distribution-id ${{secrets.DISTRIBUTION_ID}} --paths "/civil11-test/*"
